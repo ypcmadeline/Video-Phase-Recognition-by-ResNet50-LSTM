ef782cec4268ced19a0e2f3284a376fe8cf59ef3
ypcmadeline <ypchanai@connect.ust.hk>
2022-03-25 23:42:38 +0800

    commit

--- dataset.py
+++ dataset.py
@@ -13,6 +13,10 @@ from torch.utils.data import Dataset
 # from torchvision import transforms
 import glob
 import re
+import torch
+
+part =  2
+sequence_length = 3
 
 
 class Surgery(Dataset):
@@ -21,8 +25,7 @@ class Surgery(Dataset):
         self.train = train
         self.data = []
 
-        if train == True:
-        
+        if train == True:       
             files = glob.glob("datas/annotation" + "/*.csv")
             for f in files:
                 if f == "datas/annotation\\video_41.csv":
@@ -31,31 +34,16 @@ class Surgery(Dataset):
                 d = re.findall(r'\d+', f)
                 df["video"] = d[0]
                 self.data.append(df)
-                print(f)
-            
+                # print(f)         
             self.data = pd.concat(self.data)
-
-            print(sum(self.data.Phase==0))
-            print(sum(self.data.Phase==1))
-            print(sum(self.data.Phase==2))
-            print(sum(self.data.Phase==3))
-            print(sum(self.data.Phase==4))
-            print(sum(self.data.Phase==5))
-            print(sum(self.data.Phase==6))
-
             rows = self.data.values.tolist()
             # print(len(rows))
             oversampled_rows = [row for row in rows for _ in range(get_sample_ratio(row))]
             # print(len(oversampled_rows))
             self.data = pd.DataFrame(oversampled_rows, columns=self.data.columns)
-
         else:
             self.data = pd.read_csv("datas/annotation/video_41.csv")
-
-
-
-
-       
+   
 
     def __getitem__(self, index):
         """
@@ -65,16 +53,48 @@ class Surgery(Dataset):
             tuple: (sample, target) where target is class_index of the
                    target class.
         """
-        data = self.data.iloc[index]
-        if self.train == True:
-            video = data[2]
+        if part == 2:
+            length = len(self.data)
+            image = []
+            label = self.data.iloc[index][1]
+            # print(f"index: {index}")
+            for i in range(sequence_length):
+                idx = index+i
+                if idx > length-1:
+                    idx = 0
+                    index = 0
+                data = self.data.iloc[idx]
+                if self.train == True:
+                    video = data[2]
+                else:
+                    video = 41
+                img = Image.open(f"datas/{video}/{data[0]}")
+                # print(f"datas/{video}/{data[0]}")
+                if self.transform:
+                    img = self.transform(img)
+                image.append(img)
+            image = torch.stack([image[k] for k in range(len(image))], dim=0)
         else:
-            video = 41
-        image = Image.open(f"datas/images/{video}/{data[0]}")
-        if self.transform:
-            image = self.transform(image)
-        label = data[1]
+            data = self.data.iloc[index]
+            if self.train == True:
+                video = data[2]
+            else:
+                video = 41
+            image = Image.open(f"datas/{video}/{data[0]}")
+            if self.transform:
+                image = self.transform(image)
+            label = data[1]
         return image, label
+        # data = self.data.iloc[index]
+        # if self.train == True:
+        #     video = data[2]
+        # else:
+        #     video = 41
+        # image = Image.open(f"datas/{video}/{data[0]}")
+        # if self.transform:
+        #     image = self.transform(image)
+        # label = data[1]
+        # return image, label
 
     def __len__(self):
         return len(self.data)
@@ -86,20 +106,13 @@ def get_sample_ratio(row):
 
 if __name__ == '__main__':
     df = pd.read_csv(f"datas/annotation/video_1.csv")
-    # ratio = [1,1,3,4,1,15,11]
-    # rows = df_train.values.tolist()
-    # print(len(rows))
-    # oversampled_rows = [row for row in rows for _ in range(get_sample_ratio(row))]
-    # print(len(oversampled_rows))
-    # df_train_oversampled = pd.DataFrame(oversampled_rows, columns=df_train.columns)
-    # print(df_train_oversampled)
-    # # print(df_train_oversampled)
-    # files = glob.glob("datas/annotation" + "/*.csv")
-    # for f in files:
-    #     df = pd.read_csv(f)
-    #     d = re.findall(r'\d+', f)
-    #     df["video"] = d[0]
-    #     print(df)
+    print(sum(df.data.Phase==0))
+    print(sum(df.data.Phase==1))
+    print(sum(df.data.Phase==2))
+    print(sum(df.data.Phase==3))
+    print(sum(df.data.Phase==4))
+    print(sum(df.data.Phase==5))
+    print(sum(df.data.Phase==6))
     s = Surgery()
     
 
--- train.py
+++ train.py
@@ -18,10 +18,12 @@ import torchvision.models as models
 import torchvision
 from torch.utils.data import Dataset,DataLoader
 import torch.nn.functional as F
+from torch.autograd import Variable
 
 from tqdm import tqdm
 import torch.nn.init as init
-from dataset import Surgery_1
+from dataset import Surgery
+from matplotlib import pyplot as plt
 
 train_transform =transforms.Compose([
     transforms.ToTensor(),
@@ -34,15 +36,69 @@ test_transform = transforms.Compose([
     transforms.ToTensor(),
     ])
 
+use_cuda = True
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 epochs =10
 sequence_length = 3
 learning_rate = 5e-4
-loss_layer = nn.CrossEntropyLoss()
+loss_layer = nn.CrossEntropyLoss().to(device)
 
+loss_train_list = []
+loss_test_list = []
+acc_train_list = []
+acc_test_list = []
 
-def train(model,train_loader,learning_rate):  
+def plot(loss_train, loss_test, acc_train, acc_test):
+    figure, axis = plt.subplots(1, 2)
+    axis[0].plot(loss_train, label='Train')
+    axis[0].plot(loss_test, label='Valid')
+    axis[0].set_xlabel('epoch')
+    axis[0].set_ylabel('loss')
+    axis[0].set_title("Training and Validation Loss")
+    axis[1].plot(acc_train, label='Train')
+    axis[1].plot(acc_test, label='Valid')
+    axis[1].legend(['Train','Valid'])
+    axis[1].set_xlabel('epoch')
+    axis[1].set_ylabel('Accuracy')
+    axis[1].set_title("Training and Validation Accuracy")
+    plt.show()
+    plt.savefig("graph.jpg")
+
+class ResNetLSTM(nn.Module):
+    def __init__(self):
+        super(ResNetLSTM, self).__init__()
+        print("LSTM")
+        self.resnet = torchvision.models.resnet50(pretrained=True)       
+        self.resnet.fc = nn.Sequential(nn.Linear(2048, 2048))
+        ct = 0
+        for child in self.resnet.children():
+            ct += 1
+            if ct < 7:
+                for param in child.parameters():
+                    param.requires_grad = False
+
+        self.LSTM=torch.nn.LSTM(2048, 512, 3, batch_first=True)
+        self.fc=torch.nn.Linear(512,7)
     
+    def forward(self, x):
+        batch_size = x.size(0)
+        sequence = x.size(1)
+        img = torch.Tensor(batch_size,sequence,2048).cuda()
+        for s in range(sequence_length):
+            img[:,s,:] = self.resnet(x[:,s,:,:,:])
+
+        seq ,_ = self.LSTM(img)
+
+        seq = seq[:,-1,:]
+
+        out = self.fc(seq)
+
+        return out
+
+
+def train(model,train_loader, test_loader, learning_rate):  
+    model = model.to(device)
+    optimizer = optim.Adam(model.parameters(), learning_rate, weight_decay=1e-5)
     # print(learning_rate)
     for epoch in range(1, epochs + 1):
         if epoch % 2 == 0:
@@ -51,35 +107,95 @@ def train(model,train_loader,learning_rate):
         model.train()
         
         correct = 0
-        total = 0
+        total = len(train_dataloader.dataset)
         loss_item = 0
 
         for data in tqdm(train_loader):
             ## your code
-            pass
+            optimizer.zero_grad()
+
+            images, target = data
+
+            if use_cuda:
+                images = images.cuda()
+                target = target.cuda()
+
+            outputs = model(images)
+                
+            loss = loss_layer(outputs, target)
+            # loss = Variable(loss, requires_grad = True)
+            loss.backward()
+            optimizer.step()
+
+            pred = torch.argmax(outputs, dim=1)
+            loss_item += loss.item()
+            correct += torch.sum(pred == target)
+        
+        loss = loss_item / total
+        loss_train_list.append(loss)
+        acc_train = correct / total
+        acc_train_list.append(acc_train.item())
+
+        print('Train: Acc {}, Loss {}'.format(acc_train, loss))
+
+        test(model, test_loader)
+        
+        
 
            
 def test(model,test_loader):
     print('Testing...')
     model.eval()
     correct = 0
-    total = 0
+    total = len(test_dataloader.dataset)
     loss_item = 0
     with torch.no_grad():
         for data in tqdm(test_loader):
-            pass
-    print('Test: Acc {}, Loss {}'.format(correct / total, loss_item / total))
+            images, target = data
+
+            if use_cuda:
+                images = images.cuda()
+                target = target.cuda()
+
+            outputs = model(images)
+            pred = torch.argmax(outputs, dim=1)
+            loss = loss_layer(outputs, target)
+            loss_item += loss.item()
+            correct += torch.sum(pred == target)
+        
+        loss = loss_item / total
+        loss_test_list.append(loss)
+        acc_test = correct / total
+        acc_test_list.append(acc_test.item())
+
+
+    print('Test: Acc {}, Loss {}'.format(acc_test, loss))
     accuracy = correct / total
     return accuracy
 
 if __name__ == '__main__':
 
-    model = torchvision.models.resnet50(pretrained=True)
-    model.fc = nn.Sequential(nn.Linear(2048, 6))
+    part = 2
 
-    traindataset = Surgery_1(transform=train_transform)
+    if part == 2:
+        print("Running ResNet+LSTM")
+        model = ResNetLSTM()
+    else:
+        print("Running ResNet50")
+        model = torchvision.models.resnet50(pretrained=True)
+        model.fc = nn.Sequential(nn.Linear(2048, 7))   
+        ct = 0
+        for child in model.children():
+            ct += 1
+            if ct < 7:
+                for param in child.parameters():
+                    param.requires_grad = False
+
+    traindataset = Surgery(transform=train_transform)
     train_dataloader = DataLoader(traindataset, batch_size=32, shuffle=True, drop_last=True)
-    testdataset = Surgery_1(train=False, transform=train_transform)
+    testdataset = Surgery(train=False, transform=test_transform)
     test_dataloader = DataLoader(testdataset, batch_size=32, shuffle=False, drop_last=False)
     train(model, train_dataloader,test_dataloader,learning_rate)
+
+    plot(loss_train_list, loss_test_list, acc_train_list, acc_test_list)
    
